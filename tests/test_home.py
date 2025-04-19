8079535b47520c3a04cf01a4106f75cda461971e
lgustavomarinho <marinho.aprendizado@gmail.com>
2025-04-19 17:53:20 -0300

    Atualizacao para novos testes

--- tests/test_home.py
+++ tests/test_home.py
@@ -4,5 +4,5 @@ def test_home(browser):
     url = "http://127.0.0.1:8080"
     browser.get(url)
     time.sleep(10)
-    assert browser.title == "Dashboard"
+    assert browser.title == "Dash"
     print("Teste da página inicial concluído com sucesso.")
